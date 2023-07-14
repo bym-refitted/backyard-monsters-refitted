505da7c7c943004ec6d3076c6750913e5c0d5ab8
React <bymdestoyers@gmail.com>
2023-07-15 00:40:53 +0100

    Revert "Reverted MikroORM changes from develop and main"
    This reverts commit 7620c1a340d35a74a25383881f06566ef6c2bb6a.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+import { EntityManager } from '@mikro-orm/sqlite';
 import express, { Express } from "express";
 import routes from "./app.routes.js";
 import fs from "fs";
@@ -22,12 +24,13 @@ app.get("/crossdomain.xml", (_: any, res) => {
 });
 
 app.use(express.static("./public"));
-app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || port, () => {
   logging(`
   ${ascii_node} Admin dashboard: http://localhost:${port}
   `);
 });
+
+app.use(express.static(__dirname + '/public'));
 
 export default app;
