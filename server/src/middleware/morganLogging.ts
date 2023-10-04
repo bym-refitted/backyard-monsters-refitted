f4684d90c486623435b83a40983795b51d3bf43c
React <bymdestroyers@gmail.com>
2023-10-04 05:12:56 +0100

    Improved visibility of missing assets in logs | moved logAssets flag to GameSettings.ts
    This commit sees general improvements made to server logs, by now only returning 404 assets. The flag for enabling this has been moved from the .env file, to a more suitable place in GameSettings.ts

---
+++ server/src/middleware/morganLogging.ts
@@ -0,0 +1,20 @@
+import { Context } from "vm";
+import { gameConfig } from "../config/GameSettings";
+import { errorLog } from "../utils/logger";
+import morgan from "koa-morgan";
+import { Next } from "koa";
+
+export const logMissingAssets = async (ctx: Context, next: Next) => {
+  await next();
+
+  const assetURI = ctx.url.startsWith("/assets");
+
+  if (assetURI && ctx.status === 404 && gameConfig.logMissingAssets) {
+    errorLog(`${`Missing Asset:`.toUpperCase()} ${ctx.status} ${ctx.url}`);
+  }
+};
+
+const logFormat = "Server: :method :status :url";
+export const morganLogging = morgan(logFormat, {
+  skip: (ctx) => ctx.url.startsWith("/assets"),
+});
