fcb5344623b9bb0b1d95d852159dc1d346d4a391
React <bymdestroyers@gmail.com>
2024-02-25 17:18:35 +0000

    Dynamically set MikroORM debug mode based on environment

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,15 +1,15 @@
-import { MikroORM  } from '@mikro-orm/core';
-import { MariaDbDriver } from '@mikro-orm/mariadb';
-import path from 'path';
-import { Save } from './models/save.model';
-import { User } from './models/user.model';
+import { MikroORM } from "@mikro-orm/core";
+import { MariaDbDriver } from "@mikro-orm/mariadb";
+import path from "path";
+import { Save } from "./models/save.model";
+import { User } from "./models/user.model";
 
 // The configuration for the ORM - Any Entities added need to be put in here, other than that probably doesn't need to be touched
 // tslint:disable-next-line: no-object-literal-type-assertion
 export default {
-  type: 'mariadb',
+  type: "mariadb",
   allowGlobalContext: false,
-  debug: true,
+  debug: process.env.ENV !== "production" ? true : false,
   entities: [Save, User],
   dbName: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
@@ -17,7 +17,7 @@ export default {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   migrations: {
-    path: path.join(__dirname, '../src/database/migrations'),
+    path: path.join(__dirname, "../src/database/migrations"),
     pattern: /^[\w-]+\d+\.[j]s$/,
   },
 } as Parameters<typeof MikroORM.init<MariaDbDriver>>[0];
