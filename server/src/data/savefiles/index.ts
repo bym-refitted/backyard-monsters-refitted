53a81e22179abf5aef7ef9220747eee74379760b
sush1lemon <37240199+sush1lemon@users.noreply.github.com>
2024-02-27 12:14:53 +0800

    add legionnaire save file, change viximo to kongregate

--- server/src/data/savefiles/index.ts
+++ server/src/data/savefiles/index.ts
@@ -1,3 +1,4 @@
+import abunaki from "./abunaki";
 import kozu from "./kozu";
 import legionnaire from "./legionnaire";
 
@@ -5,4 +6,4 @@ import legionnaire from "./legionnaire";
  * Implement more base save and levels
  */
 
-export default [legionnaire, kozu, legionnaire, legionnaire]
+export default [legionnaire, kozu, abunaki, legionnaire]
