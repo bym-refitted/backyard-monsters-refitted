a1c9cd2a4a51a62448b85fa20404541dc1fb9eac
React <bymdestroyers@gmail.com>
2024-04-05 19:28:30 +0100

    Added Portuguese language support (50%)

--- server/src/controllers/supportedLangs.ts
+++ server/src/controllers/supportedLangs.ts
@@ -1,7 +1,7 @@
 import { KoaController } from "../utils/KoaController";
 
 export const supportedLangs: KoaController = async (ctx) => {
-  const languages = ["English"];
+  const languages = ["English", "Portuguese"];
 
   ctx.status = 200;
   ctx.body = languages;
