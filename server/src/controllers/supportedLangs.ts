c26a685cd0cab30ea137232b01a98e7809fe9683
React <bymdestroyers@gmail.com>
2024-04-09 23:28:16 +0100

    Added French translations (50%)

--- server/src/controllers/supportedLangs.ts
+++ server/src/controllers/supportedLangs.ts
@@ -1,7 +1,7 @@
 import { KoaController } from "../utils/KoaController";
 
 export const supportedLangs: KoaController = async (ctx) => {
-  const languages = ["English", "Spanish", "Portuguese"];
+  const languages = ["English", "French", "Spanish", "Portuguese"];
 
   ctx.status = 200;
   ctx.body = languages;
