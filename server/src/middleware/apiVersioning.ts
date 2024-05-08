7df64f1c79fd448857773646e6268e3925a22712
React <bymdestroyers@gmail.com>
2024-05-08 22:10:35 +0100

    Fixed API Versioning middleware

--- server/src/middleware/apiVersioning.ts
+++ server/src/middleware/apiVersioning.ts
@@ -3,12 +3,8 @@ import {getApiVersion} from "../server";
 
 export const apiVersion = async (ctx: Context, next: Next) => {
     const apiVersion = ctx.params.apiVersion;
-
-    // console.log("apiVersion from params",apiVersion);
-    
     const expectedApiVersion = getApiVersion();
-    console.log("apiVersion from ctx", expectedApiVersion);
-    
+    // console.log("apiVersion from ctx", expectedApiVersion);
     // Check if the apiVersion is valid
     if (process.env.USE_VERSION_MANAGEMENT === "enabled" && apiVersion !== expectedApiVersion) {
         ctx.status = 400;
@@ -17,5 +13,5 @@ export const apiVersion = async (ctx: Context, next: Next) => {
     }
 
     // If the apiVersion is valid, continue to the next middleware
-    return next();
+    await next();
 }
