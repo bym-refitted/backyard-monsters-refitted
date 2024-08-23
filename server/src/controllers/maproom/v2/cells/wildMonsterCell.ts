bbdd93f26cdb4df7b1c8a2b755c1842cc09bdcac
React <bymdestroyers@gmail.com>
2024-08-23 22:59:07 +0100

    Updated wildMonsterCell.ts to load necessary WM data

--- server/src/controllers/maproom/v2/cells/wildMonsterCell.ts
+++ server/src/controllers/maproom/v2/cells/wildMonsterCell.ts
@@ -1,52 +1,18 @@
 import { WorldMapCell } from "../../../../models/worldmapcell.model";
-import { ORMContext } from "../../../../server";
-import { Save } from "../../../../models/save.model";
-import { generateBaseID } from "../../../../services/maproom/v2/world";
+import { Tribes } from "../../../../enums/Tribes";
 
-const tribes = ["Legionnaire", "Kozu", "Abunakki", "Dreadnaut"];
-
-export const wildMonsterCell = async (
-  terrainType: number,
-  cell: WorldMapCell,
-  level: number = 32
-) => {
-  // Q: Is this a pseudo-random tribe assignment?
-  const tribe = (cell.x + cell.y) % tribes.length;
-  const tribeName = tribes[tribe];
-
-  let save: Save = null;
-  if (cell.base_id !== 0) {
-    save = await ORMContext.em.findOne(Save, {
-      baseid: cell?.base_id.toString(),
-      wmid: {
-        $ne: 0,
-      },
-    });
-  }
+export const wildMonsterCell = async (cell: WorldMapCell) => {
+  const tribeIndex = (cell.x + cell.y) % Tribes.length;
+  let baseId = 1000000 + cell.y + cell.x * 1000;
 
   return {
-    uid: cell?.uid || 0, // ToDo: Why do we have a userId for both user and save table? Fix
+    uid: baseId,
     b: 1,
-    fbid: "",
-    pi: 0,
-    bid: generateBaseID(parseInt(cell.world_id), cell.x, cell.y),
-    aid: 0,
-    i: terrainType,
-    mine: 0,
-    f: save?.flinger || 0,
-    c: save?.catapult || 0,
-    t: 0,
-    n: tribeName,
-    fr: 0,
-    on: 0, // ToDo
-    p: 0,
-    r: save?.resources || 0,
-    m: save?.monsters || 0,
-    l: save?.level || level, // ToDo
-    d: save?.destroyed || 0,
-    lo: save?.locked || 0,
-    dm: save?.damage || 0,
-    pic_square: "",
-    im: "",
+    i: cell.terrainHeight,
+    bid: baseId,
+    n: Tribes[tribeIndex],
+    l: 40, // TODO: Implement level
+    dm: 0, // TODO: Implement damage
+    d: 0, // TODO: Implement Base destroyed
   };
 };
