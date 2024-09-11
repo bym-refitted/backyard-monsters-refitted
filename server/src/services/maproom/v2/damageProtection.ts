2a369ca329628fb802317627c75bd20bea8e02db
React <bymdestroyers@gmail.com>
2024-09-11 18:31:28 +0100

    Added damage protection TODO scenarios in damageProtection.ts

--- server/src/services/maproom/v2/damageProtection.ts
+++ server/src/services/maproom/v2/damageProtection.ts
@@ -14,9 +14,17 @@ export const damageProtection = async (save: Save, mode?: BaseMode) => {
   else {
     switch (type) {
       case BaseType.MAIN:
+        // Protection scenarios:
+        // 1. First week of the game = 7d
+        // 2. 4 attacks in 1 hour = 1hr
+        // 3. 25-50% damage = 18h
+        // 4. 50% or more damage = 36h
         if (damage >= 50) isCellProtected = 1;
         break;
       case BaseType.OUTPOST:
+        // Protection scenarios:
+        // 1. First takeover = 12h
+        // 2. 25% damage in 2-3 attacks, instant protection on third attack = 8h
         if (damage >= 25) isCellProtected = 1;
         break;
       default:
