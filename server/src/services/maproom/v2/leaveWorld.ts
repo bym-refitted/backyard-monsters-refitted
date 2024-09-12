de0d252543bea8f78565ccf7eeb32e805ccd3c42
React <bymdestroyers@gmail.com>
2024-09-12 19:32:40 +0100

    Decrement player count on a world when a user leaves

--- server/src/services/maproom/v2/leaveWorld.ts
+++ server/src/services/maproom/v2/leaveWorld.ts
@@ -1,27 +1,36 @@
 import { BaseType } from "../../../enums/Base";
 import { Save } from "../../../models/save.model";
 import { User } from "../../../models/user.model";
+import { World } from "../../../models/world.model";
 import { WorldMapCell } from "../../../models/worldmapcell.model";
 import { ORMContext } from "../../../server";
 
 /**
  * Handles the process of a user leaving a world.
- * Resets the user's save data associated with a world
- * & removes their cells from the world.
+ *
+ * Decrements the player count of the world the user is leaving.
+ * Resets the user's save data associated with a world & removes their cells.
  *
  * @param {User} user - The user who is leaving the world.
  * @param {Save} save - The save data associated with the user.
  */
 export const leaveWorld = async (user: User, save: Save) => {
+  const world = await ORMContext.em.findOne(World, { uuid: save.worldid });
+
+  // Decrement the player count
+  world.playerCount -= 1;
+
+  // Reset the user's save data
   save.worldid = "";
+  save.usemap = 0;
   save.cell = null;
   save.homebase = null;
   save.outposts = [];
-  save.usemap = 0;
-  user.bookmarks = null;
+  user.bookmarks = {};
 
+  // Remove user's cells from the world
   await removeUserCells(user);
-  await ORMContext.em.persistAndFlush([save, user]);
+  await ORMContext.em.persistAndFlush([world, save, user]);
 };
 
 const removeUserCells = async (user: User) => {
