835f97136c68c823a84211eadfa20bf7fdc7db0d
React <bymdestroyers@gmail.com>
2024-10-05 04:25:09 +0100

    Basic implementation for migrating base | Commit has bugs

--- server/src/controllers/maproom/v2/migrateBase.ts
+++ server/src/controllers/maproom/v2/migrateBase.ts
@@ -1,83 +1,106 @@
+import z from "zod";
+
 import { KoaController } from "../../../utils/KoaController";
 import { User } from "../../../models/user.model";
 import { ORMContext } from "../../../server";
-import { Save } from "../../../models/save.model";
-import { joinOrCreateWorld } from "../../../services/maproom/v2/joinOrCreateWorld";
 import { WorldMapCell } from "../../../models/worldmapcell.model";
 import { Status } from "../../../enums/StatusCodes";
-import { Operation, updateResources } from "../../../services/base/updateResources";
+import { BaseType } from "../../../enums/Base";
+import {
+  Operation,
+  updateResources,
+} from "../../../services/base/updateResources";
+import { getCurrentDateTime } from "../../../utils/getCurrentDateTime";
+import { errorLog } from "../../../utils/logger";
 
-// TODO: Rewrite
-interface MigrateBaseRequest {
-    shiny?: string,
-    baseid: string,
-    type: string,
-    resources?: string
-}
+const MigrateBaseSchema = z.object({
+  type: z.string(),
+  baseid: z.string().transform((baseid) => parseInt(baseid)),
+  resources: z.string().transform((res) => JSON.parse(res)).optional(),
+  shiny: z.string().transform((shiny) => parseInt(shiny)).optional(),
+});
+
+// Wiki: https://backyardmonsters.fandom.com/wiki/Jumping
 export const migrateBase: KoaController = async (ctx) => {
-    console.log("migrating base", ctx.request.body)
-    const request = <MigrateBaseRequest>ctx.request.body;
-    const user: User = ctx.authUser;
-    await ORMContext.em.populate(user, ["save"]);
-    const save: Save = user.save;
-    let error = 0;
+  try {
+    const { baseid, resources, shiny, type } = MigrateBaseSchema.parse(
+      ctx.request.body
+    );
 
-    if (request.resources) {
-        save.resources = updateResources(JSON.parse(request.resources), save.resources || {}, Operation.SUBTRACT);
-    } else if (request.shiny) {
-        const credits = parseInt(request.shiny)
-        if (save.credits - credits < 0) {
-            error = 1;
-        } else {
-            save.credits = Math.max(0, save.credits - credits)
-        }
+    const currentUser: User = ctx.authUser;
+    await ORMContext.em.populate(currentUser, ["save"]);
+
+    const userSave = currentUser.save;
+
+    // Fetch the outpost cell which the user is migrating to
+    const outpostCell = await ORMContext.em.findOne(
+      WorldMapCell,
+      {
+        base_id: baseid,
+      },
+      { populate: ["save"] }
+    );
+
+    // Error migrating user base: Error: Invalid base or base type. Base ID: 1005799
+    // Steps: "You have moved your main yard already" popup, then try again
+    if (!outpostCell || !outpostCell.save) {
+      ctx.status = Status.BAD_REQUEST;
+      ctx.body = { error: 1 };
+      throw new Error(`Invalid base or base type. Base ID: ${baseid}`);
     }
 
-    if (request.type === "outpost") {
-        const homecell = await ORMContext.em.findOne(WorldMapCell, {
-            base_id: save.homebaseid,
-        })
+    if (shiny) userSave.credits = userSave.credits - shiny;
+    if (resources) 
+        userSave.resources = updateResources(resources, userSave.resources, Operation.SUBTRACT);
 
-        const cell = await ORMContext.em.findOne(WorldMapCell, {
-            base_id: parseInt(request.baseid, 10)
-        })
-        const outpost = await ORMContext.em.findOne(Save, {
-            baseid: request.baseid
-        })
+    // TODO: Handle relocate logic
+    // await joinOrCreateWorld(currentUser, userSave, ORMContext.em, true);
+    if (type != BaseType.OUTPOST) throw new Error("Relocate logic not implemented");
 
-        if (homecell && cell && outpost) {
-            save.homebase = [cell.x.toString(), cell.y.toString()]
-            save.outposts = save.outposts.filter(o => o[2] != parseInt(outpost.baseid))
-            delete save.buildingresources[`b${outpost.baseid}`]
-            homecell.x = cell.x
-            homecell.y = cell.y
-            await Promise.all([
-                ORMContext.em.removeAndFlush(cell),
-                ORMContext.em.removeAndFlush(outpost),
-                ORMContext.em.persistAndFlush(homecell),
-                ORMContext.em.persistAndFlush(save)
-            ])
-        }
-    } else {
-        await joinOrCreateWorld(user, save, ORMContext.em, true);
+    // Fetch the user's homecell
+    const homeCell = await ORMContext.em.findOne(WorldMapCell, {
+      base_id: userSave.homebaseid,
+    });
+
+    if (!homeCell) {
+      ctx.status = Status.BAD_REQUEST;
+      ctx.body = { error: 1 };
+      throw new Error("Invalid home cell");
     }
 
-    //
-    const outposts = await ORMContext.em.find(Save, {
-        homebaseid: save.homebaseid,
-        type: "outpost",
-    })
+    // Update user's homebase and coordinates to the outpost cell
+    userSave.homebase = [outpostCell.x.toString(), outpostCell.y.toString()];
+    homeCell.x = outpostCell.x;
+    homeCell.y = outpostCell.y;
 
-    outposts.map(base => {
-        base.homebase = save.homebase
-        return base;
-    })
-    // Update outposts homebase
-    await ORMContext.em.persistAndFlush(outposts);
+    // Remove the outpost from the user's save, 3rd element in the array is the baseid
+    userSave.outposts = userSave.outposts.filter(
+      (outpost) => outpost[2] !== baseid
+    );
 
+    // Remove baseid from building resources object
+    delete userSave.buildingresources[`b${outpostCell.save.baseid}`];
+
+    await Promise.all([
+      ORMContext.em.removeAndFlush(outpostCell),
+      ORMContext.em.persistAndFlush(userSave),
+    ]);
+
+    const currentTime = getCurrentDateTime();
+    const cantMoveTill = currentTime + 24 * 60 * 60; // 24 hours
+
+    // TODO: Notify homecell and all outposts that outpost has been removed
+    // TODO: Look at PopupLostMainBase.as and the properties it expects
     ctx.status = Status.OK;
     ctx.body = {
-        error: error,
-        resources: save.resources
-    }
-}
+      error: 0,
+      resources: userSave.resources,
+      currenttime: currentTime,
+      cantMoveTill,
+    };
+  } catch (error) {
+    ctx.status = Status.BAD_REQUEST;
+    ctx.body = { error: 1 };
+    errorLog("Error migrating user base:", error);
+  }
+};
--- server/src/controllers/maproom/v2/takeoverCell.ts
+++ server/src/controllers/maproom/v2/takeoverCell.ts
@@ -11,6 +11,7 @@ import {
   Operation,
   updateResources,
 } from "../../../services/base/updateResources";
+import { errorLog } from "../../../utils/logger";
 
 const TakeoverCellSchema = z.object({
   baseid: z.string().transform((baseid) => parseInt(baseid)),
@@ -22,57 +23,67 @@ const TakeoverCellSchema = z.object({
  * Controller to handle the takeover of a cell on the world map via shiny or resources.
  *
  * Retrieve the cell that is being taken over, by querying it with the baseid from the client.
- * To transfer ownership, update properties on the user, user's save, the cell and the associated cell save. 
+ * To transfer ownership, update properties on the user, user's save, the cell and the associated cell save.
  * @param {Context} ctx - The Koa context object.
  * @throws Will throw an error if the base or base type is invalid.
  */
 export const takeoverCell: KoaController = async (ctx) => {
-  const { baseid, resources, shiny } = TakeoverCellSchema.parse(ctx.request.body);
+  try {
+    const { baseid, resources, shiny } = TakeoverCellSchema.parse(
+      ctx.request.body
+    );
 
-  const currentUser: User = ctx.authUser;
-  await ORMContext.em.populate(currentUser, ["save"]);
+    const currentUser: User = ctx.authUser;
+    await ORMContext.em.populate(currentUser, ["save"]);
 
-  const userSave = currentUser.save;
+    const userSave = currentUser.save;
 
-  const cell = await ORMContext.em.findOne(
-    WorldMapCell,
-    {
-      base_id: baseid,
-    },
-    { populate: ["save"] }
-  );
+    const cell = await ORMContext.em.findOne(
+      WorldMapCell,
+      {
+        base_id: baseid,
+      },
+      { populate: ["save"] }
+    );
 
-  if (!cell || !cell.save || cell.save.type === BaseType.MAIN) {
+    if (!cell || !cell.save || cell.save.type === BaseType.MAIN) {
+      ctx.status = Status.BAD_REQUEST;
+      ctx.body = { error: 1 };
+      throw new Error(`Invalid base or base type. Base ID: ${baseid}`);
+    }
+
+    const cellSave = cell.save;
+
+    if (shiny) userSave.credits = userSave.credits - shiny;
+    if (resources) 
+      userSave.resources = updateResources(resources, userSave.resources, Operation.SUBTRACT);
+
+    // Update save
+    cellSave.saveuserid = currentUser.userid;
+    cellSave.homebaseid = userSave.homebaseid;
+    cellSave.homebase = [cell.x.toString(), cell.y.toString()];
+
+    if (cellSave.type === BaseType.TRIBE) {
+      cellSave.type = BaseType.OUTPOST;
+      cellSave.buildingdata = {};
+      cellSave.wmid = 0;
+    }
+
+    // Update cell
+    cell.uid = currentUser.userid;
+    cell.base_type = MapRoomCell.OUTPOST;
+
+    // Update user
+    userSave.outposts.push([cell.x, cell.y, baseid]);
+    ORMContext.em.persistAndFlush([cellSave, currentUser]);
+
+    // TODO: Notify homecell and all outposts that outpost has been added
+
+    ctx.status = Status.OK;
+    ctx.body = { error: 0 };
+  } catch (error) {
     ctx.status = Status.BAD_REQUEST;
-    ctx.body = { error: 1, message: "Invalid base or base type." };
-    throw new Error(`Invalid base or base type. Base ID: ${baseid}`);
+    ctx.body = { error: 1 };
+    errorLog("Error taking over cell:", error);
   }
-
-  const { save: cellSave } = cell;
-
-  if (shiny) userSave.credits = userSave.credits - shiny;
-  if (resources) 
-    userSave.resources = updateResources(resources, userSave.resources, Operation.SUBTRACT);
-
-  // Update save
-  cellSave.saveuserid = currentUser.userid;
-  cellSave.homebaseid = userSave.homebaseid;
-  cellSave.homebase = [cell.x.toString(), cell.y.toString()];
-
-  if (cellSave.type === BaseType.TRIBE) {
-    cellSave.type = BaseType.OUTPOST;
-    cellSave.buildingdata = {};
-    cellSave.wmid = 0;
-  }
-
-  // Update cell
-  cell.uid = currentUser.userid;
-  cell.base_type = MapRoomCell.OUTPOST;
-
-  // Update user
-  userSave.outposts.push([cell.x, cell.y, baseid]);
-  ORMContext.em.persistAndFlush([cellSave, currentUser]);
-
-  ctx.status = Status.OK;
-  ctx.body = { error: 0 };
 };
