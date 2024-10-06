8799baae1cc4a42a8844b65838fd5aef552d2a72
React <bymdestroyers@gmail.com>
2024-10-06 16:34:21 +0100

    Rewrote 'transferAssets.ts'

---
+++ server/src/controllers/maproom/v2/transferMonsters.ts
@@ -0,0 +1,55 @@
+import z from "zod";
+
+import { KoaController } from "../../../utils/KoaController";
+import { ORMContext } from "../../../server";
+import { Save } from "../../../models/save.model";
+import { Status } from "../../../enums/StatusCodes";
+
+interface Monster {
+  hid: number[];
+  saved: number;
+  housed: Record<string, number>;
+  hstage: number[];
+}
+
+type MonstersTransfer = [Monster[], Monster[]];
+
+const TransferMonstersScema = z.object({
+  frombaseid: z.string(),
+  tobaseid: z.string(),
+  monsters: z.string().transform((monsters) => JSON.parse(monsters)),
+});
+
+/**
+ * Controller to handle the transfer of monsters between outposts and main yards.
+ *
+ * @param {Object} ctx - The Koa context object.
+ * @returns {Promise<void>}
+ */
+export const transferMonsters: KoaController = async (ctx) => {
+  const { frombaseid, tobaseid, monsters } = TransferMonstersScema.parse(
+    ctx.request.body
+  );
+
+  const [fromMonsters, toMonsters]: MonstersTransfer = monsters;
+
+  const [fromBase, toBase] = await ORMContext.em.find(Save, {
+    baseid: { $in: [frombaseid, tobaseid] },
+  });
+
+  if (!fromBase || !toBase) {
+    ctx.status = Status.BAD_REQUEST;
+    ctx.body = { error: 1 };
+    throw new Error(`One or both bases not found. From: ${frombaseid}, To: ${tobaseid}`);
+  }
+
+  fromBase.monsters = fromMonsters;
+  toBase.monsters = toMonsters;
+
+  await ORMContext.em.persistAndFlush([fromBase, toBase]);
+
+  ctx.status = Status.OK;
+  ctx.body = {
+    error: 0,
+  };
+};
