8b7c0bc10634bbb6041b9bb9e59baef8fd05d92d
React <bymdestroyers@gmail.com>
2024-10-09 16:20:40 +0100

    Additional logic improvements to migrateBase.ts

--- server/src/controllers/maproom/v2/migrateBase.ts
+++ server/src/controllers/maproom/v2/migrateBase.ts
@@ -24,23 +24,23 @@ const MigrateBaseSchema = z.object({
   shiny: z.string().transform((shiny) => parseInt(shiny)).optional(),
 });
 
-/** 
- * Cooldown period for base migration. 
+/**
+ * Cooldown period for base migration.
  * @constant {number}
- */ 
-const COOLDOWN_PERIOD = 24 * 60 * 60; 
+ */
+const COOLDOWN_PERIOD = 24 * 60 * 60;
 
 /**
  * Handles user base migration.
- * 
+ *
  * This controller allows a user to migrate their base to a new location.
  * It checks if the user is within the cooldown period, validates the base being migrated to
  * and relocates their home base. If the migration is successful, the user's cooldown is updated
  * and the new home base is saved.
- * 
+ *
  * @param {Object} ctx - The Koa context object
  * @returns {Promise<void>} A promise that resolves once the base migration is complete.
- * 
+ *
  * @throws Will throw an error if the base type is invalid, no homebase is found or if it catches an exception.
  */
 export const migrateBase: KoaController = async (ctx) => {
@@ -55,6 +55,9 @@ export const migrateBase: KoaController = async (ctx) => {
     const userSave = currentUser.save;
     const currentTime = getCurrentDateTime();
 
+    if (shiny) userSave.credits = userSave.credits - shiny;
+    if (resources) userSave.resources = updateResources(resources, userSave.resources, Operation.SUBTRACT);
+
     // Check if the user is within the cooldown period.
     if (userSave.cantmovetill > currentTime) {
       ctx.status = Status.OK;
@@ -66,7 +69,7 @@ export const migrateBase: KoaController = async (ctx) => {
       return;
     }
 
-    // Check if the user is relocating due to 'empire destroyed'.
+    // Check if the user is relocating due to their empire being overrun.
     if (type !== BaseType.OUTPOST && baseid === BaseMode.DEFAULT) {
       await joinOrCreateWorld(currentUser, userSave, ORMContext.em, true);
       ctx.status = Status.OK;
@@ -74,14 +77,17 @@ export const migrateBase: KoaController = async (ctx) => {
       return;
     }
 
-    // Otherwise, fetch the outpost cell which the user is migrating to.
-    const outpostCell = await ORMContext.em.findOne(
+    // Otherwise, fetch the outpost cell (destination) and the user's homeCell (origin) for migration.
+    const cells = await ORMContext.em.find(
       WorldMapCell,
       {
-        base_id: parseInt(baseid),
+        base_id: { $in: [parseInt(baseid), userSave.homebaseid] },
       },
       { populate: ["save"] }
     );
+
+    const outpostCell = cells.find((cell) => cell.base_id === parseInt(baseid));
+    const homeCell = cells.find((cell) => cell.base_id === userSave.homebaseid);
 
     if (!outpostCell || !outpostCell.save) {
       ctx.status = Status.BAD_REQUEST;
@@ -89,22 +95,13 @@ export const migrateBase: KoaController = async (ctx) => {
       throw new Error(`Invalid base or base type. Base ID: ${baseid}`);
     }
 
-    if (shiny) userSave.credits = userSave.credits - shiny;
-    if (resources)
-      userSave.resources = updateResources(resources, userSave.resources, Operation.SUBTRACT);
-
-    // Fetch the user's homecell
-    const homeCell = await ORMContext.em.findOne(WorldMapCell, {
-      base_id: userSave.homebaseid,
-    });
-
     if (!homeCell) {
       ctx.status = Status.BAD_REQUEST;
       ctx.body = { error: 1 };
       throw new Error("Invalid home cell");
     }
-    
-    // Update the user's homecell to the outpost cell
+
+    // Update the user's homecell coordinates to the outpost cell
     homeCell.x = outpostCell.x;
     homeCell.y = outpostCell.y;
     homeCell.terrainHeight = outpostCell.terrainHeight;
@@ -117,18 +114,19 @@ export const migrateBase: KoaController = async (ctx) => {
 
     // Remove the outpost from the user's save, 3rd element in the array is the baseid
     userSave.outposts = userSave.outposts.filter((outpost) => 
-      outpost[2] !== parseInt(baseid));
+      outpost[2] !== parseInt(baseid)
+    );
 
     // Remove baseid from building resources object
     delete userSave.buildingresources[`b${outpostCell.save.baseid}`];
 
     await ORMContext.em.removeAndFlush([outpostCell.save, outpostCell]);
     await ORMContext.em.persistAndFlush([homeCell, userSave]);
-    
+
     ctx.status = Status.OK;
     ctx.body = {
       error: 0,
-      resources: userSave.resources
+      resources: userSave.resources,
     };
   } catch (error) {
     ctx.status = Status.BAD_REQUEST;
