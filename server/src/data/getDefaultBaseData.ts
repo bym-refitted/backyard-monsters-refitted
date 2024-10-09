7d59b597256044fcca587eb0c421885bb23472fb
React <bymdestroyers@gmail.com>
2024-10-09 09:14:07 +0100

    Moved majority of default save data directly to the save model using defaults values

--- server/src/data/getDefaultBaseData.ts
+++ server/src/data/getDefaultBaseData.ts
@@ -19,65 +19,18 @@ export const getDefaultBaseData = (user?: User) => {
 
   const baseid = generateID(9);
 
-  // TODO: Move more stuff to the model
   return {
     baseid: baseid.toString(10),
-    type: "main",
-    userid: generateID(8),
-    wmid: 0,
-    seed: 0,
     saveuserid: user.userid,
-    bookmarked: 0,
-    createtime: getCurrentDateTime(),
-    savetime: 0, // Updates each time a save is triggered
-    fan: 0,
-    emailshared: 0,
-    unreadmessages: 0,
-    giftsentcount: 0,
-    id: 0, // Gets set as same value as savetime when save is triggered
-    canattack: false,
+    userid: user.userid,
     cellid: -1,
-    baseid_inferno: 0,
-    fbid: "",
-    fortifycellid: 0,
-    name: user.username || "Anonymous",
-    level: 1,
-    catapult: 0,
-    flinger: 0,
-    destroyed: 0,
-    damage: 0,
-    locked: 0,
-    points: 0,
-    basevalue: 20,
-    protected: 1,
-    lastupdate: 0,
-    usemap: 0,
     homebaseid: baseid,
+    baseid_inferno: 0,
+    name: user.username,
     credits: devConfig.shiny || 1000,
-    champion: "null",
-    empiredestroyed: 0,
-    worldid: "",
-    event_score: 0,
-    chatenabled: 0,
-    relationship: 0,
-    error: 0,
-    user,
+    createtime: getCurrentDateTime(),
 
-    // Objects
-    buildingdata: {},
-    buildingkeydata: {},
-    buildinghealthdata: {},
-    researchdata: {},
-    lockerdata: {},
-    aiattacks: {},
-    mushrooms: {},
-    stats: {},
-    academy: {},
-    monsterbaiter: {},
-    loot: {},
-    storedata: {},
-    coords: {},
-    quests: {},
+    // Pre-populated Objects
     resources: {
       r1: 0,
       r2: 0,
@@ -88,31 +41,6 @@ export const getDefaultBaseData = (user?: User) => {
       r3max: 10000,
       r4max: 10000,
     },
-    inventory: {},
-    monsters: {},
-    player: {},
-    krallen: devConfig.unlockAllEventRewards
-      ? {
-          countdown: 443189,
-          wins: 5,
-          tier: 5,
-          loot: 750000000000,
-        }
-      : {},
-    siege: {},
-    buildingresources: {},
-    frontpage: {},
-    events: {},
-    rewards: devConfig.unlockAllEventRewards
-      ? {
-          // Unique event reward unlockables
-          unlockRezghul: { id: Reward.REZGHUL },
-          unblockSlimeattikus: { id: Reward.SLIMEATTIKUS },
-          unblockVorg: { id: Reward.VORG },
-          krallenReward: { id: Reward.KRALLEN, value: 1 },
-        }
-      : {},
-    takeover: {},
     iresources: {
       r1: 0,
       r2: 0,
@@ -123,42 +51,22 @@ export const getDefaultBaseData = (user?: User) => {
       r3max: 10000,
       r4max: 10000,
     },
-
-    // Arrays
-    savetemplate: [],
-    updates: [],
-    effects: [],
-    homebase: null,
-    outposts: [],
-    wmstatus: [],
-    chatservers: ["bym-chat.kixeye.com"],
-    powerups: [], // ToDo: add to DB
-    attpowerups: [], // ToDo: add to DB
-
-    // These properties do not get returned in base load, why are they here?
-    version: 128,
-    baseseed: 0,
-    healtime: 0,
-    empirevalue: 0,
-    clienttime: 0,
-    timeplayed: 0,
-    achieved: [],
-    monsterupdate: {},
-    basename: "basename",
-    attackreport: "",
-    over: 1,
-    protect: 0,
-    attackid: 0,
-    attacks: [],
-    gifts: [],
-    sentinvites: [],
-    sentgifts: [],
-    attackcreatures: {},
-    attackloot: {},
-    lootreport: {},
-    attackerchampion: "null",
-    attackersiege: {},
-    purchasecomplete: 0,
-    fbpromos: [],
+    krallen: devConfig.unlockAllEventRewards
+      ? {
+          countdown: 443189,
+          wins: 5,
+          tier: 5,
+          loot: 750000000000,
+        }
+      : {},
+    rewards: devConfig.unlockAllEventRewards
+      ? {
+          // Unique event reward unlockables
+          unlockRezghul: { id: Reward.REZGHUL },
+          unblockSlimeattikus: { id: Reward.SLIMEATTIKUS },
+          unblockVorg: { id: Reward.VORG },
+          krallenReward: { id: Reward.KRALLEN, value: 1 },
+        }
+      : {}
   };
 };
