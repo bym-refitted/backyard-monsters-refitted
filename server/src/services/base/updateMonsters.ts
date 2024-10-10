8deba33df3b7bc91a68834379631bb9427c9ecb1
React <bymdestroyers@gmail.com>
2024-10-10 17:57:01 +0100

    Reverted some breaking changes with monster updates

--- server/src/services/base/updateMonsters.ts
+++ server/src/services/base/updateMonsters.ts
@@ -1,26 +1,30 @@
-import { Monster } from "../../controllers/base/save/zod/BaseSaveSchema";
 import { Save } from "../../models/save.model";
 import { ORMContext } from "../../server";
 
-// TODO: This needs to be tested, how do we trigger this?
-// export const updateMonsters = async (monsterupdates: Monster) => {
-//   console.log("Monster Update Bases", monsterupdates);
+interface MonsterUpdate {
+  baseid: string;
+  m: any;
+}
+
+// This needs to be tested, how do we trigger this?
+export const updateMonsters = async (monsterupdates: MonsterUpdate[]) => {
+  console.log("Monster Update Bases", monsterupdates);
   
-//   // Fetch all bases that match the provided base IDs in one go
-//   const baseIds = monsterupdates.map((update) => update.baseid);
-//   const saves = await ORMContext.em.find(Save, { baseid: { $in: baseIds } });
+  // Fetch all bases that match the provided base IDs in one go
+  const baseIds = monsterupdates.map((update) => update.baseid);
+  const saves = await ORMContext.em.find(Save, { baseid: { $in: baseIds } });
 
-//   // Iterate over the saves and apply the updates
-//   for (const save of saves) {
-//     const monsterUpdate = monsterupdates.find(
-//       (update) => update.baseid === save.baseid
-//     );
+  // Iterate over the saves and apply the updates
+  for (const save of saves) {
+    const monsterUpdate = monsterupdates.find(
+      (update) => update.baseid === save.baseid
+    );
 
-//     if (monsterUpdate) {
-//       save.protected = 0;
-//       save.monsters = monsterUpdate.m;
-//     }
-//   }
+    if (monsterUpdate) {
+      save.protected = 0;
+      save.monsters = monsterUpdate.m;
+    }
+  }
 
-//   await ORMContext.em.flush();
-// };
+  await ORMContext.em.flush();
+};
