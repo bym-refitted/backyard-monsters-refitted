b30792f28d889080309fb5feba58a72faf927862
React <bymdestroyers@gmail.com>
2024-10-14 15:29:33 +0100

    Fixes for production build

--- server/src/config/DevSettings.ts
+++ server/src/config/DevSettings.ts
@@ -1,9 +1,11 @@
+import { Env } from "../enums/Env";
+
 /** Visit our Wiki to get more information on each flag.
  * Wiki: https://github.com/bym-refitted/backyard-monsters-refitted/wiki/Dev-Settings-%E2%80%90-Configuration
  */
 export const devConfig = {
   logMissingAssets: false,
-  skipTutorial: true,
+  skipTutorial: process.env.ENV !== Env.PROD,
   unlockAllEventRewards: true,
   maproom: true,
   inferno: false,
