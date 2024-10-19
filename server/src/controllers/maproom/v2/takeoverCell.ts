ef13e1dd05c60d81c206bdf097e7e7cbfff7a7fe
React <bymdestroyers@gmail.com>
2024-10-19 23:21:11 +0100

    Outpost takeover now reflects on previous owner's outpost list

--- server/src/controllers/maproom/v2/takeoverCell.ts
+++ server/src/controllers/maproom/v2/takeoverCell.ts
@@ -24,6 +24,8 @@ const TakeoverCellSchema = z.object({
  *
  * Retrieve the cell that is being taken over, by querying it with the baseid from the client.
  * To transfer ownership, update properties on the user, user's save, the cell and the associated cell save.
+ * The previous owner's outposts are updated to reflect the takeover.
+ * 
  * @param {Context} ctx - The Koa context object.
  * @throws Will throw an error if the base or base type is invalid.
  */
@@ -55,8 +57,26 @@ export const takeoverCell: KoaController = async (ctx) => {
     const cellSave = cell.save;
 
     if (shiny) userSave.credits = userSave.credits - shiny;
-    if (resources) 
+    if (resources)
       userSave.resources = updateResources(resources, userSave.resources, Operation.SUBTRACT);
+
+    // Find the previous owner
+    const previousOwner = await ORMContext.em.findOne(
+      User,
+      { userid: cellSave.userid },
+      { populate: ["save"] }
+    );
+
+    if (previousOwner?.save) {
+      const { outposts } = previousOwner.save;
+
+      // Filter out the outpost that matches the specified cell
+      previousOwner.save.outposts = outposts.filter(
+        ([x, y, id]) => !(x === cell.x && y === cell.y && id === baseid)
+      );
+
+      await ORMContext.em.persistAndFlush(previousOwner);
+    }
 
     // Update save
     cellSave.saveuserid = currentUser.userid;
