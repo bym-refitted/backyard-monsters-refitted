98eb0f4f9e252516251c0ab989dbc09ef9b7d2dd
React <bymdestroyers@gmail.com>
2024-12-17 03:20:19 +0000

    Fixes to register.ts

--- server/src/controllers/auth/register.ts
+++ server/src/controllers/auth/register.ts
@@ -3,14 +3,9 @@ import { KoaController } from "../../utils/KoaController";
 import { ORMContext } from "../../server";
 import { User } from "../../models/user.model";
 import { FilterFrontendKeys } from "../../utils/FrontendKey";
-import {
-  authFailureErr,
-  emailUniqueErr,
-  usernameUniqueErr,
-} from "../../errors/errors";
+import { emailUniqueErr, usernameUniqueErr } from "../../errors/errors";
 import { logging } from "../../utils/logger";
 import { Status } from "../../enums/StatusCodes";
-import { ClientSafeError } from "../../middleware/clientSafeError";
 import { UserRegistrationSchema } from "./zod/AuthSchemas";
 
 /**
@@ -25,46 +20,39 @@ import { UserRegistrationSchema } from "./zod/AuthSchemas";
  * @throws {Error} - Throws an error if registration fails or if the request body is invalid.
  */
 export const register: KoaController = async (ctx) => {
-  try {
-    const registeredUser = UserRegistrationSchema.parse(ctx.request.body);
+  const registeredUser = UserRegistrationSchema.parse(ctx.request.body);
 
-    // Find user by username or email
-    const existingUser = await ORMContext.em.findOne(User, {
-      $or: [
-        { username: registeredUser.username },
-        { email: registeredUser.email },
-      ],
-    });
+  // Find user by username or email
+  const existingUser = await ORMContext.em.findOne(User, {
+    $or: [
+      { username: registeredUser.username },
+      { email: registeredUser.email },
+    ],
+  });
 
-    // If user exists, check if username or email is already taken
-    if (existingUser) {
-      const isUsernameTaken = existingUser.username === registeredUser.username;
-      const isEmailTaken = existingUser.email === registeredUser.email;
+  // If user exists, check if username or email is already taken
+  if (existingUser) {
+    const isUsernameTaken = existingUser.username === registeredUser.username;
+    const isEmailTaken = existingUser.email === registeredUser.email;
 
-      if (isUsernameTaken) throw usernameUniqueErr();
-      if (isEmailTaken) throw emailUniqueErr();
-    }
-
-    const hash = await bcrypt.hash(registeredUser.password, 10);
-
-    // Create new user record
-    const user = ORMContext.em.create(User, {
-      ...registeredUser,
-      password: hash,
-    });
-
-    await ORMContext.em.persistAndFlush(user);
-    const filteredUser = FilterFrontendKeys(user);
-    logging(
-      `User ${filteredUser.username} registered successfully | ID: ${filteredUser.userid} | Email: ${filteredUser.email} | IP Address: ${ctx.ip}`
-    );
-
-    ctx.status = Status.OK;
-    ctx.body = { user: filteredUser };
-  } catch (err) {
-    if (err instanceof ClientSafeError) {
-      ctx.status = err.status;
-      ctx.body = { message: err.message, code: err.code };
-    } else throw authFailureErr();
+    if (isUsernameTaken) throw usernameUniqueErr();
+    if (isEmailTaken) throw emailUniqueErr();
   }
+
+  const hash = await bcrypt.hash(registeredUser.password, 10);
+
+  // Create new user record
+  const user = ORMContext.em.create(User, {
+    ...registeredUser,
+    password: hash,
+  });
+
+  await ORMContext.em.persistAndFlush(user);
+  const filteredUser = FilterFrontendKeys(user);
+  logging(
+    `User ${filteredUser.username} registered successfully | ID: ${filteredUser.userid} | Email: ${filteredUser.email} | IP Address: ${ctx.ip}`
+  );
+
+  ctx.status = Status.OK;
+  ctx.body = { user: filteredUser };
 };
