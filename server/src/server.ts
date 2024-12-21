0f38967130224c1398f0bcd1bd09826c2d13044f
React <bymdestroyers@gmail.com>
2024-12-21 17:46:41 +0000

    Bumped API version to v1.1.1

--- server/src/server.ts
+++ server/src/server.ts
@@ -44,7 +44,7 @@ export const setApiVersion = (version: string) => {
   globalApiVersion = version;
 };
 
-export const getApiVersion = () => "v1.1.0-beta";
+export const getApiVersion = () => "v1.1.1-beta";
 export const PORT = process.env.PORT || 3001;
 export const BASE_URL = process.env.BASE_URL;
 
