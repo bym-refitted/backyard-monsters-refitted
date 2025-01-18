d1ec1789962eb5196a356bd21138952bea9ea7d8
React <bymdestroyers@gmail.com>
2025-01-18 17:45:13 +0000

    Additional cleanup for baseSave.ts

--- server/src/enums/SaveKeys.ts
+++ server/src/enums/SaveKeys.ts
@@ -6,7 +6,6 @@
 export enum SaveKeys {
   RESOURCES = "resources",
   BUILDING_RESOURCES = "buildingresources",
-  BUILDING_HEALTH_DATA = "buildinghealthdata",
   PURCHASE = "purchase",
   ACADEMY = "academy",
   CHAMPION = "champion",
