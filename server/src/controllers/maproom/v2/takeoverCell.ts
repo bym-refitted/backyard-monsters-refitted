dccd76bf7b2f409fc776b21585acf7f7addf8f3a
React <bymdestroyers@gmail.com>
2025-01-19 21:25:14 +0000

    Set tutorial stage to finished for outposts

--- server/src/controllers/maproom/v2/takeoverCell.ts
+++ server/src/controllers/maproom/v2/takeoverCell.ts
@@ -100,6 +100,7 @@ export const takeoverCell: KoaController = async (ctx) => {
     cellSave.protected = 1;
     cellSave.attackTimestamps = [];
     cellSave.resources = {};
+    cellSave.tutorialstage = 205;
 
     if (cellSave.type === BaseType.TRIBE) {
       cellSave.type = BaseType.OUTPOST;
