bb6d0d895f7ec8db1cd496010c5a6786a074ec07
React <bymdestroyers@gmail.com>
2025-03-01 08:41:31 +0000

    Added Moloch tribe 209

--- server/src/config/DevSettings.ts
+++ server/src/config/DevSettings.ts
@@ -36,7 +36,7 @@ export const devConfig = {
    * Inserts an Inferno sandbox test base into the database, with all buildings placed.
    * Must be set before creating a new record.
    */
-  infernoSandbox: false,
+  infernoSandbox: true,
 
   /*
    * Logs all missing assets and their paths to the server console.
