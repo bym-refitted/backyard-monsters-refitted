08e49d705928890cd2343f25b02f212b28c8651e
React <bymdestroyers@gmail.com>
2025-03-07 13:11:19 +0000

    Reverted DevSettings

--- server/src/config/DevSettings.ts
+++ server/src/config/DevSettings.ts
@@ -35,13 +35,13 @@ export const devConfig = {
    * Inserts a sandbox test base into the database, with all buildings placed.
    * Must be set before creating a new record.
    */
-  devSandbox: true,
+  devSandbox: false,
 
   /*
    * Inserts an Inferno sandbox test base into the database, with all buildings placed.
    * Must be set before creating a new record.
    */
-  infernoSandbox: true,
+  infernoSandbox: false,
 
   /*
    * Logs all missing assets and their paths to the server console.
