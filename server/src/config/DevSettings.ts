73aaceb3d60d3e5ab078c5424b56c0eb1d45774c
React <bymdestroyers@gmail.com>
2025-03-07 13:18:17 +0000

    Improved DevSettings.ts to always set flags as false in prod for critical flags

--- server/src/config/DevSettings.ts
+++ server/src/config/DevSettings.ts
@@ -29,19 +29,19 @@ export const devConfig = {
    * Enable or disable the debug console. Requires a client restart.
    * A list of all available commands can be found in `ConsoleCommands.as`
    */
-  debugMode: false,
+  debugMode: process.env.ENV === Env.PROD ? false : false,
 
   /*
    * Inserts a sandbox test base into the database, with all buildings placed.
    * Must be set before creating a new record.
    */
-  devSandbox: false,
+  devSandbox: process.env.ENV === Env.PROD ? false : false,
 
   /*
    * Inserts an Inferno sandbox test base into the database, with all buildings placed.
    * Must be set before creating a new record.
    */
-  infernoSandbox: false,
+  infernoSandbox: process.env.ENV === Env.PROD ? false : false,
 
   /*
    * Logs all missing assets and their paths to the server console.
@@ -57,7 +57,7 @@ export const devConfig = {
   /*
    * Sets whether the descent into Inferno should be enabled or disabled.
    */
-  skipDescent: true,
+  skipDescent: process.env.ENV === Env.PROD ? false : false,
 
   /*
    * Sets whether the tutorial phase of the game is enabled or disabled.
