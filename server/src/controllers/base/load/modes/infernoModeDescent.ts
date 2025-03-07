9403f4c8609de6527df9004b5a69ee0d489a3736
React <bymdestroyers@gmail.com>
2025-03-07 12:34:30 +0000

    Removed unneccesary call to persistAndFlush from infernoModeDescent.ts

--- server/src/controllers/base/load/modes/infernoModeDescent.ts
+++ server/src/controllers/base/load/modes/infernoModeDescent.ts
@@ -7,7 +7,7 @@ import { ORMContext } from "../../../../server";
 
 export const infernoModeDescent = async (user: User) => {
   const { userid } = user.save;
-  
+
   let baseSave = await ORMContext.em.findOne(Save, {
     userid,
     type: BaseType.MAIN,
@@ -17,17 +17,17 @@ export const infernoModeDescent = async (user: User) => {
 
   if (!maproom1) await MapRoom1.setupMapRoom1Data(ORMContext.em, user);
 
-  // If the user already has descent tribes, return the save.
-  if (baseSave.wmstatus.length !== 0) return baseSave;
-
   // Otherwise, create an array of 13 descent tribes, client expects IDs between 201-213.
   const tribes = Array.from({ length: 13 }, (_, i) => [201 + i, i + 1, 0]);
 
   // Skip descent if the devConfig flag is set.
   if (devConfig.skipDescent) tribes.forEach((tribe) => (tribe[2] = 1));
 
+  // If the user already has descent tribes, return the save.
+  if (baseSave.wmstatus.length !== 0) return baseSave;
+
   baseSave.wmstatus = tribes;
-  await ORMContext.em.persistAndFlush(baseSave);
+  await ORMContext.em.flush();
 
   return baseSave;
 };
