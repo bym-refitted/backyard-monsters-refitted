0ffb1bc8d9b8200da3966e231577cf020f687f27
React <bymdestroyers@gmail.com>
2025-03-28 22:52:47 +0000

    Experimental fix: Outpost regular kits are in building phase when attacked for the 2nd time

--- server/src/controllers/base/load/baseLoad.ts
+++ server/src/controllers/base/load/baseLoad.ts
@@ -94,6 +94,7 @@ export const baseLoad: KoaController = async (ctx) => {
       storeitems: { ...storeItems },
       tutorialstage: isTutorialEnabled,
       currenttime: getCurrentDateTime(),
+      savetime: getCurrentDateTime(),
       pic_square: `${process.env.AVATAR_URL}?seed=${
         filteredSave.name
       }&size=${50}`,
