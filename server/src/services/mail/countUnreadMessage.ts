a05861f8fab9bbc4305d8c1615e0d9a69cf577cf
senninr <181501909+senninr@users.noreply.github.com>
2025-04-07 20:35:47 +0700

    move message.model db call logic to service (#144)

---
+++ server/src/services/mail/countUnreadMessage.ts
@@ -0,0 +1,17 @@
+import { Message } from "../../models/message.model";
+import { ORMContext } from "../../server";
+
+export const countUnreadMessage = (id: number) => {
+    return ORMContext.em.count(Message, {
+        $or: [
+            {
+                userid: id,
+                userUnread: 1,
+            },
+            {
+                targetid: id,
+                targetUnread: 1,
+            },
+        ],
+    });
+}
