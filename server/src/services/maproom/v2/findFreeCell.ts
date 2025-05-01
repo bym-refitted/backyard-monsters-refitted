ba819ec46c19c4d8cf71224cb654493a60089fa3
React <bymdestroyers@gmail.com>
2025-05-01 20:38:10 +0100

    Moved generateMap.ts into services module

--- server/src/services/maproom/v2/findFreeCell.ts
+++ server/src/services/maproom/v2/findFreeCell.ts
@@ -3,7 +3,7 @@ import { World } from "../../../models/world.model";
 import { WorldMapCell } from "../../../models/worldmapcell.model";
 import { EntityManager } from "@mikro-orm/core";
 import { logging } from "../../../utils/logger";
-import { generateNoise, getTerrainHeight } from "../../../controllers/maproom/v2/generateMap";
+import { generateNoise, getTerrainHeight } from "./generateMap";
 
 /**
  * Interface representing a single cell
