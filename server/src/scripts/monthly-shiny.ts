694ea60182297a76b41306dc548a7ea226f47e21
React <bymdestroyers@gmail.com>
2025-05-20 14:15:53 +0100

    Updated JSDoc comments in monthly-shiny script for how-to-use

--- server/src/scripts/monthly-shiny.ts
+++ server/src/scripts/monthly-shiny.ts
@@ -1,9 +1,17 @@
-import 'dotenv/config'; 
+import "dotenv/config";
 import mikroOrmConfig from "../mikro-orm.config";
 
 import { MikroORM } from "@mikro-orm/core";
 import { Save } from "../models/save.model";
 
+/**
+ * This script is responsible for adding shiny to all main yard saves.
+ *
+ * To run this script on a production server (using pm2):
+ * 1) cd into the server directory
+ * 2) run the command:
+ * `pm2 start dist/scripts/monthly-shiny.js --cron "0 13 20 * *" --name "monthly-shiny" --no-autorestart`
+ */
 (async () => {
   try {
     const shinyAmount = 400;
@@ -14,7 +22,7 @@ import { Save } from "../models/save.model";
     const saves = await em.find(Save, { type: "main" });
 
     console.log(`Adding ${shinyAmount} credits to each save...`);
-    
+
     for (const save of saves) {
       save.credits += shinyAmount;
       save.monthly_credits += shinyAmount;
