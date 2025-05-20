7feac7f1d8d9a8a0bc1d21df04f50f1ec384e22c
React <bymdestroyers@gmail.com>
2025-05-20 14:13:15 +0100

    Updated monthly-shiny script

--- server/src/scripts/monthly-shiny.ts
+++ server/src/scripts/monthly-shiny.ts
@@ -1,13 +1,20 @@
+import 'dotenv/config'; 
+import mikroOrmConfig from "../mikro-orm.config";
+
+import { MikroORM } from "@mikro-orm/core";
 import { Save } from "../models/save.model";
-import { ORMContext } from "../server";
 
 (async () => {
   try {
     const shinyAmount = 400;
-    const em = ORMContext.em.fork();
+
+    const orm = await MikroORM.init(mikroOrmConfig);
+    const em = orm.em.fork();
 
     const saves = await em.find(Save, { type: "main" });
 
+    console.log(`Adding ${shinyAmount} credits to each save...`);
+    
     for (const save of saves) {
       save.credits += shinyAmount;
       save.monthly_credits += shinyAmount;
@@ -16,6 +23,7 @@ import { ORMContext } from "../server";
     await em.flush();
     console.log(`Updated ${saves.length} save(s) with +${shinyAmount} credits`);
 
+    await orm.close();
     process.exit(0);
   } catch (error) {
     console.error("Failed to run monthly-shiny job:", error);
