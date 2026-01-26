4e52d032bf0c0912124d49ed84348523f2188810
React1-X <reactx.bym@gmail.com>
2026-01-26 21:57:48 +0000

    Cleaned up server-wrapper.js

--- server/server-wrapper.js
+++ server/server-wrapper.js
@@ -8,7 +8,4 @@
  *
  * @see https://github.com/oven-sh/bun/issues/19942#issuecomment-3297830230
  */
-import("./src/server.ts").catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+import("./src/server.ts").catch(() => process.exit(1));
