73126c9bec26a33edafd084aa453f0c54bc106c5
React1-X <reactx.bym@gmail.com>
2026-02-15 17:18:20 +0000

    Updated code comments

--- server/src/scripts/anticheat/anticheat.ts
+++ server/src/scripts/anticheat/anticheat.ts
@@ -2,12 +2,17 @@ import { Save } from '../../models/save.model.js';
 import { User } from '../../models/user.model.js';
 import { Env } from '../../enums/Env.js';
 
-let antiCheatModule: { validateSave: (user: User, save: Save, rawBody: unknown) => Promise<void> } | null = null;
+interface AntiCheatModule {
+  initialize: () => Promise<void>;
+  validateSave: (user: User, save: Save, rawBody: unknown) => Promise<void>;
+}
+
+let antiCheatModule: AntiCheatModule | null = null;
 
 async function loadAntiCheatModule() {
   if (antiCheatModule) return antiCheatModule;
 
-  if (process.env.ENV !== Env.PROD) 
+  if (process.env.ENV !== Env.PROD)
     return antiCheatModule = await import('./pub/anticheat.stub.js');
 
   try {
@@ -19,6 +24,11 @@ async function loadAntiCheatModule() {
 
   return antiCheatModule;
 }
+
+export const initAnticheat = async () => {
+  const module = await loadAntiCheatModule();
+  await module.initialize();
+};
 
 export const validateSave = async (user: User, save: Save, rawBody: unknown) => {
   const module = await loadAntiCheatModule();
