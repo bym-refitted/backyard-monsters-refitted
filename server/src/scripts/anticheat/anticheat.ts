6729b6366acb86914584b6a13dfbde10f56fecb6
React1-X <reactx.bym@gmail.com>
2026-02-15 00:23:01 +0000

    anticheat does not trigger in non-prod environments

--- server/src/scripts/anticheat/anticheat.ts
+++ server/src/scripts/anticheat/anticheat.ts
@@ -1,10 +1,14 @@
 import { Save } from '../../models/save.model.js';
 import { User } from '../../models/user.model.js';
+import { Env } from '../../enums/Env.js';
 
 let antiCheatModule: { validateSave: (user: User, save: Save) => Promise<void> } | null = null;
 
 async function loadAntiCheatModule() {
   if (antiCheatModule) return antiCheatModule;
+
+  if (process.env.ENV !== Env.PROD) 
+    return antiCheatModule = await import('./anticheat.stub.js');
 
   try {
     // @ts-ignore: private module may not exist in development builds
