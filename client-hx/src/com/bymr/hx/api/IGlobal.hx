2d70a6901562badc332079c732be69b1f4fe1abf
Jannick Oursin <github@joursin.com>
2026-02-18 00:43:08 +0100

    implement build pipeline for building the haxe SWC library and added documentation

---
+++ client-hx/src/com/bymr/hx/api/IGlobal.hx
@@ -0,0 +1,8 @@
+package com.bymr.hx.api;
+
+/**
+ * This interface defines methods that the Haxe Code can call on the GLOBAL class in AS3.
+ */
+interface IGlobal {
+    function log(message: String): Void;    
+}
