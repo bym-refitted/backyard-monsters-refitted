a4df50ce2e81996460fac98b217774d1e1e9ccdd
Jannick Oursin <github@joursin.com>
2026-02-18 01:08:17 +0100

    refactor: enhance IGlobal interface documentation for log method

--- client-hx/src/com/bymr/hx/api/IGlobal.hx
+++ client-hx/src/com/bymr/hx/api/IGlobal.hx
@@ -4,5 +4,12 @@ package com.bymr.hx.api;
  * This interface defines methods that the Haxe Code can call on the GLOBAL class in AS3.
  */
 interface IGlobal {
-    function log(message: String): Void;    
+	/**
+	 * Logs a message to the console. This allows Haxe code to log messages through the AS3 GLOBAL class.
+	 * 
+	 * This is just an example method. You can add more methods here that you want to be able to call from Haxe code on the GLOBAL class in AS3.
+	 * 
+	 * @param message The message to log.
+	 */
+	function log(message:String):Void;
 }
