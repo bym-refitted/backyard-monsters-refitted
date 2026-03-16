1b7c089dc68373deee6065a4f361fdbcf5b41878
React1-X <reactx.bym@gmail.com>
2026-03-16 05:00:54 +0000

    Added MR3 relocation feature

--- server/src/app.routes.ts
+++ server/src/app.routes.ts
@@ -10,6 +10,7 @@ import { getMapRoomCells } from "./controllers/maproom/v3/getCells.js";
 import { getNewMap } from "./controllers/maproom/getNewMap.js";
 import { verifyUserAuth, verifyAccountStatus } from "./middleware/auth.js";
 import { relocate } from "./controllers/maproom/v3/relocate.js";
+import { getFriendInfo } from "./controllers/maproom/v3/getFriendInfo.js";
 import { infernoMonsters } from "./controllers/inferno/infernoMonsters.js";
 import { recordDebugData } from "./controllers/debug/recordDebugData.js";
 import { getTemplates } from "./controllers/yardplanner/getTemplates.js";
@@ -361,15 +362,16 @@ router.post(
 
 /**
  * Worldmap v3 relocate base
- * @name POST /worldmapv3/relocate
+ * @name GET /worldmapv3/relocate
  */
-router.post(
-  "/worldmapv3/relocate",
-  verifyUserAuth,
-  verifyAccountStatus,
-  logRequest("Relocating MR3 base"),
-  relocate
-);
+router.get("/worldmapv3/relocate", verifyUserAuth, verifyAccountStatus, logRequest("Relocating MR3 base"), relocate);
+
+
+/**
+ * Worldmap v3 friend info for relocation popup
+ * @name GET POST /worldmapv3/getfriendinfo
+ */
+router.get("/worldmapv3/getfriendinfo", verifyUserAuth, verifyAccountStatus, getFriendInfo);
 
 /**
  * Worldmap v3 set map version
