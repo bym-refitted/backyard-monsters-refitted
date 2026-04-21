cb079de2e7508edd1159aab3ff1b1e993d16eefb
React1-X <reactx.bym@gmail.com>
2026-04-22 00:46:09 +0100

    Fixed bug which showed player online across all owned cells

--- server/src/controllers/base/load/modes/baseModeAttack.ts
+++ server/src/controllers/base/load/modes/baseModeAttack.ts
@@ -60,11 +60,12 @@ export const baseModeAttack = async ({ user, baseid, mapversion, attackCost }: B
   if (!save) throw new Error(`Save not found for baseid: ${baseid}`);
 
   if (save.type !== BaseType.TRIBE) {
-    const lastSeen = await redis.get(`last-seen:${BaseType.MAIN}:${save.userid}`);
-    
-    if (lastSeen && parseInt(lastSeen) >= getCurrentDateTime() - 60) throw userOnlineErr();
-
     if (isAttackActive(save)) throw baseUnderAttackErr();
+
+    if (save.type === BaseType.MAIN) {
+      const lastSeen = await redis.get(`last-seen:${BaseType.MAIN}:${save.userid}`);
+      if (lastSeen && parseInt(lastSeen) >= getCurrentDateTime() - 60) throw userOnlineErr();
+    }
   }
 
   if (save.attacks.length > 3) save.attacks = save.attacks.slice(-2);
