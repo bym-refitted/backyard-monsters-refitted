b8bef0be38fa5cf111d31b8be9a5b576fa28972f
React1-X <reactx.bym@gmail.com>
2026-04-25 17:01:00 +0100

    v1.6.1-beta release

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,7 +23,7 @@ app.proxy = true;
 export const PORT = process.env.PORT || 3001;
 export const BASE_URL = process.env.BASE_URL;
 
-export const getApiVersion = () => "v1.6.0-beta";
+export const getApiVersion = () => "v1.6.1-beta";
 
 export const postgres = {} as {
   orm: MikroORM<PostgreSqlDriver>;
