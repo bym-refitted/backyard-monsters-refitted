2bd578cf7a6182c8022daf5c456043c930a561d2
React1-X <reactx.bym@gmail.com>
2026-04-27 06:17:36 +0100

    Cleaned up app.routes.ts

--- server/src/middleware/logRequest.ts
+++ server/src/middleware/logRequest.ts
@@ -11,18 +11,17 @@ import { Env } from "../enums/Env.js";
  * @param {string} [logMessage=""] - Optional label for the log entry.
  * @returns {Function} Koa middleware function.
  */
-export const logRequest = (logMessage: string = "") =>
-  async (ctx: Context, next: Next) => {
-    if (process.env.ENV === Env.LOCAL) {
-      console.log("=".repeat(70));
-      console.log(`📦 ${ctx.method} ${ctx.path}${logMessage ? ` | ${logMessage}` : ""}`);
+export const logRequest = async (ctx: Context, next: Next) => {
+  if (process.env.ENV === Env.LOCAL) {
+    console.log("=".repeat(70));
+    console.log(`📦 ${ctx.method} ${ctx.path}`);
 
-      if (ctx.request.body && Object.keys(ctx.request.body).length > 0) {
-        console.log();
-        console.log(inspect(ctx.request.body, { colors: true, depth: 5, compact: false }));
-      }
-
-      console.log("=".repeat(70) + "\n");
+    if (ctx.request.body && Object.keys(ctx.request.body).length > 0) {
+      console.log();
+      console.log(inspect(ctx.request.body, { colors: true, depth: 5, compact: false }));
     }
-    await next();
-  };
+
+    console.log("=".repeat(70) + "\n");
+  }
+  await next();
+};
