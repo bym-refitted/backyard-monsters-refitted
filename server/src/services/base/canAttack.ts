787ba12cf638a6c6ce57efdecbf97f9899c209a6
React1-X <reactx.bym@gmail.com>
2026-05-18 22:25:38 +0100

    Raised resource outpost level restriction to 32 from 30

--- server/src/services/base/canAttack.ts
+++ server/src/services/base/canAttack.ts
@@ -23,7 +23,7 @@ export const canAttack = (attackerSave: Save, defenderSave: Save, mapversion?: M
    */
   if (
     mapversion === MapRoomVersion.V3 &&
-    attackerLevel >= 30 &&
+    attackerLevel >= 36 &&
     defenderSave.wmid === EnumYardType.RESOURCE &&
     defenderSave.level <= 20
   ) return false;
